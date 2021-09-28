cdcc2c875bf8acb2b377111c1f57811e252f0406
unknown <rajputnikita5666@gmail.com>
2021-09-28 15:41:42 +0530

    Added:Welcome message

---
+++ src/com/bz/linecomparision/LineComparision.java
@@ -0,0 +1,12 @@
+package com.bz.linecomparision;
+
+public class LineComparision {
+
+	public static void main(String[] args) {
+		//welcome message
+		
+		System.out.println("Welcome Line Comparision");
+
+	}
+
+}
